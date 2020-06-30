1e155124d8ca1292b7177902d57fb2e86655aeaa
Jeferson Luis <jgjefersonluis@gmail.com>
2020-06-30 16:48:51 -0300

    dict e conjuntos

---
+++ secao3-pythonintermediario/aula06-conjuntos/main.py
@@ -0,0 +1,6 @@
+# add (adiciona), update (atualiza), clear, discard
+# union | (une)
+# intersection & (todos os elementos presentes nos dois sets)
+# difference - (elementos apenas no set da esquerda)
+# symmetric_difference ^ (Elementos que estão nos dois sets, mas não em ambos
+
