4de3cf52672a38f7c2b68938e43e526d2ff54da7
Jeferson Luis <jgjefersonluis@gmail.com>
2020-07-16 02:29:19 -0300

    heranca e sobreposicao

---
+++ secao4-poo/aula10-heranca/main.py
@@ -0,0 +1,11 @@
+from classes import *
+
+"""
+Associação - Usa | Agragação - Tem | Composição - É dono | Herança É
+"""
+
+c1 = Cliente('Luis', 45)
+print (c1.nome)
+
+a1 = Aluno('Maria', 54)
+print (a1.nome)
---
+++ secao4-poo/aula11-sobreposicao/main.py
@@ -0,0 +1,15 @@
+from classes import *
+
+"""
+Associação - Usa | Agragação - Tem | Composição - É dono | Herança É
+"""
+
+c1 = Cliente('Luis', 45)
+c1.falar()
+
+print()
+
+c2 = ClienteVIP('Rose', 25, 'Miranda')
+c2.falar()
+
+
