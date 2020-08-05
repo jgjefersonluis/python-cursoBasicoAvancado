626cb755a851f66dc877cfb5b0eb48fc407b75e5
Jeferson Luis <jgjefersonluis@gmail.com>
2020-08-05 12:42:22 -0300

    conversor de videos, json, csv, random

---
+++ secao5-modulos/aula04-ffmpeg-conversorvideo/main.py
@@ -0,0 +1,73 @@
+# https://ffmpeg.zeranoe.com/builds/
+
+import os
+import fnmatch
+import sys
+
+if sys.platform == 'linux':
+    comando_ffmpeg = 'ffmpeg'
+else:
+    comando_ffmpeg = r'ffmpeg\bin\ffmpeg.exe'
+
+codec_video = '-c:v libx264'
+crf = '-crf 23'
+present = '-preset ultrafast'
+codec_audio = '-c:a aac'
+bitrate_audio = '-b:a 320k'
+debug = '-ss 00:00:00 -to 00:00:10'
+debug = ''
+
+caminho_origem ='c:/Jeferson_Historia2020/Mundo/eua/historiaEUA'
+caminho_destino = 'c:/Jeferson_Historia2020/Mundo/eua/historiaEUA'
+
+
+for raiz, pasta, arquivos in os.walk(caminho_origem):
+    for arquivo in arquivos:
+        if not fnmatch.fnmatch(arquivo, '*.mkv'):
+            continue
+
+        caminho_completo = os.path.join(raiz, arquivo)
+        nome_arquivo, extensao_arquivo = os.path.splitext(caminho_completo)
+        caminho_legenda = nome_arquivo + '.srt'
+
+        if os.path.isfile(caminho_legenda):
+            input_legenda = f'-i "{caminho_legenda}"'
+            map_legenda = '-c:s srt -map v:0 -map a -map 1:0'
+        else:
+            input_legenda = ''
+            map_legenda = ''
+
+        nome_arquivo, extensao_arquivo = os.path.splitext(arquivo)
+
+        nome_novo_arquivo = nome_arquivo + '_NOVO' + extensao_arquivo
+        arquivo_saida = os.path.join(raiz, nome_novo_arquivo)
+
+        comando = f'{comando_ffmpeg} -i "{caminho_completo}"' \
+                  f'{codec_video} {crf} {present} {codec_audio} {bitrate_audio}'\
+                  f'{debug} {map_legenda} "{arquivo_saida}"'
+
+        os.system(comando)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
