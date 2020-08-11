c66209a87fb6d11e6230b0cc8640a2abca20cf14
Jeferson Luis <jgjefersonluis@gmail.com>
2020-08-11 12:13:03 -0300

    pdf,xls e pillow

---
+++ secao5-modulos/aula16-fifo/main.py
@@ -0,0 +1,10 @@
+"""
+Pilhas e filas
+Pilhas(stack) - LIFO - last in, first out.
+    Exemplo: Pihas de livros que são adicionados um sobre o outro
+Fila (queue) - FIFO - first in, first out.
+    Exemplo: Uma fia de banco (ou qualquer fila da vida real)
+Filas podem ter efeitos colaterais em desempenho, porque a cada item
+alterado, todos os indices serão modificados.
+"""
+
